da650bac264e7da27bf7dcae8ab089c3f712a4e9
Kerl0s-dev <kezyah.larcher@outlook.fr>
2026-03-15 13:55:13 +0100

    Add startup debug logs and update icon
    Log a debug message when the mod initializes on the client and when the server loads a world to confirm the mod is loaded. Adds a static import of Indigo.LOGGER in MobHUD and a @Shadow @Final Logger in the MobHUDMixin where LOGGER.debug(...) is called. Also updates the mod icon asset (icon.png).

--- src/main/java/com/kerlos/mobhud/MobHUD.java
+++ src/main/java/com/kerlos/mobhud/MobHUD.java
@@ -4,11 +4,15 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
 import net.minecraft.util.Identifier;
 
+import static net.fabricmc.fabric.impl.client.indigo.Indigo.LOGGER;
+
 public class MobHUD implements ClientModInitializer {
 	public static final String MOD_ID = "mobhud";
 
 	@Override
 	public void onInitializeClient() {
+		LOGGER.debug("Mob Hud successfully loaded");
+
 		HudElementRegistry.addLast(
 				Identifier.of(MOD_ID, "entity_hud"),
 				(drawContext, tickCounter) -> {
